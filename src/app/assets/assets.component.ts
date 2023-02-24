5ee31790726baddc92b22dbc9a2f2e9d7efb097b
Wasim Sayyed <wasimxprt@gmail.com>
2023-02-24 22:05:54 +0530

    Adding assets data to the table

--- src/app/assets/assets.component.ts
+++ src/app/assets/assets.component.ts
@@ -3,25 +3,29 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import DerivAPIBasic from "@deriv/deriv-api/dist/DerivAPIBasic";
+import { WebSocket } from "ws";
+
+
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  // position: number;
+  // weight: number;
+  // symbol: string;
 }
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+// ];
 
 
 @Component({
@@ -30,10 +34,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./assets.component.css']
 })
 export class AssetsComponent implements OnInit, AfterViewInit {
- 
+
   title = 'socketio-angular';
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['name'];
+  dataSource = new MatTableDataSource<PeriodicElement>([]);
 
   constructor(private socketService: SocketioService, private _liveAnnouncer: LiveAnnouncer) { }
 
@@ -45,7 +49,15 @@ export class AssetsComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    //this.socketService.setupSocketConnection();
+
+    const api = new DerivAPIBasic({ app_id: 1089 });
+    api.assetIndex().then((response) => {
+      if (response.error) {
+        console.log("Error ", response.error)
+      }
+     
+      this.dataSource = new MatTableDataSource<PeriodicElement>(response.asset_index.filter((item:any) => item[0].includes("frx")));
+    });
   }
 
   ngOnDestroy() {
